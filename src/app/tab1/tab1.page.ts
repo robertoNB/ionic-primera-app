59284163e4fb1457718d57ac69134b5f9e7513ad
robertoNB <15980595@itroque.edu.mx>
2021-12-14 19:32:52 -0600

    fin sesion 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
+
+ngOnInit(): void {
+  //instruccion para detenerse en donde se encuentra parado, tambien se puede poner el brackpoint
+  debugger;
+
+  let numero = 10;
+  numero = 20;
+  numero = numero + 10;
+    console.log('Hola Mundo el numero es: ',numero);
+    
+}
 
 }
